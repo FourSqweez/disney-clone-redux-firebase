d998d64d8f2d85dddfd6eab0b41878d76f2dedef
FourSqweez <four7518@gmail.com>
2021-05-08 21:37:07 +0700

    Added : styled navMenu when hover to show border bottom from left to right

--- src/components/Header.js
+++ src/components/Header.js
@@ -66,6 +66,26 @@ const NavMenu = styled.div`
     span {
       font-size: 13px;
       letter-spacing: 1.42px;
+      position: relative;
+
+      :after {
+        content: '';
+        height: 2px;
+        position: absolute;
+        background-color: white;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform-origin: left center;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        transform: scale(0);
+      }
+
+      :hover:after {
+        opacity: 1;
+        transform: scale(1);
+      }
     }
   }
 `
