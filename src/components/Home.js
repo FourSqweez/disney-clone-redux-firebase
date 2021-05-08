f0ba84f114bdb28a77de1c794d3be8f2426c5f03
FourSqweez <four7518@gmail.com>
2021-05-08 22:55:18 +0700

    Added : overflow-x: hidden;

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,23 @@
 import styled from 'styled-components'
+import ImgSlider from './ImgSlider'
 
 export default function Home() {
-  return <Container>Home</Container>
+  return (
+    <Container>
+      <ImgSlider />
+    </Container>
+  )
 }
 
 const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position: relative;
+  overflow-x: hidden;
 
   ::before {
-    background: url('/images/home-background.png') center center / cover no-repeat
-      fixed;
+    background: url('/images/home-background.png') center center / cover
+      no-repeat fixed;
     content: '';
     position: absolute;
     top: 0;
