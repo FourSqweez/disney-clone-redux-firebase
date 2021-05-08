4dcd2d02755901dba35f6c53fd6042a720d9614d
FourSqweez <four7518@gmail.com>
2021-05-08 21:35:40 +0700

    Added : Header components

---
+++ src/components/Header.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components'
+
+export default function Header() {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src="https://scontent.fnak1-1.fna.fbcdn.net/v/t1.6435-9/142329626_2349282661884094_1479215446098487803_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=AQqahgRUp74AX_3MIeI&_nc_ht=scontent.fnak1-1.fna&oh=0b162db5d7ac9b40b989b41771edaecb&oe=60BDAD64" />
+    </Nav>
+  )
+}
+
+const Nav = styled.nav`
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`
+
+const Logo = styled.img`
+  width: 80px;
+`
+
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+
+    img {
+      height: 20px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+    }
+  }
+`
+const UserImg = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  cursor: pointer;
+  object-fit: cover;
+`
