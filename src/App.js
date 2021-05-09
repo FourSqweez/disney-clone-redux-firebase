b733e04d1af0fb8274d08bc6599df10c55fefd58
FourSqweez <four7518@gmail.com>
2021-05-09 16:11:53 +0700

    Added : Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
-import Home from './components/Home';
+import Home from './components/Home'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Detail from './components/Detail'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/detail">
+            <Detail />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
